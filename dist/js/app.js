40e252fe08fde6576852f3227b055592286c1703
ThaiNgan <truongthaingan19@gmail,com>
2019-11-27 13:11:07 +0700

    version0.3

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,19 +2,20 @@ const btn_number = document.querySelectorAll(".btn-number");
 const input = document.querySelector('.console');
 const btn_cal = document.querySelectorAll('.btn-cal');
 const history = document.querySelector('.history');
-input.value = "0";
+
+input.value = "";
 var string_number="";
-var number2=0;
+
 btn_number.forEach(element => {
     element.addEventListener('click', function(){
         let number = parseInt(element.textContent); 
-        input.value = "" + (parseInt(input.value + number));
+        input.value = "" + (parseFloat(input.value + number));
     });
 });
 
 btn_cal.forEach(element => {
     element.addEventListener('click', function(){
-        // if(check() == 0){
+        if(check() == 0 || input.value != ""){
             string_number += input.value;
             clear_number();
             let value = element.textContent;
@@ -22,6 +23,17 @@ btn_cal.forEach(element => {
                 value = "*";
             string_number += value;
             history.textContent = string_number;
+        }
+        else{
+            string_number += input.value;
+            clear_number();
+            let value = element.textContent;
+            if(value == "X")
+                value = "*";
+            string_number = remove_last(string_number) + value;
+            history.textContent = string_number;
+            console.log("remove"); 
+        }
     });
 });
 
@@ -34,7 +46,8 @@ const btn_clear = document.querySelector(".btn-clear");
 btn_clear.addEventListener('click', function() {
     clear_number();
     string_number = "";
-    input.value = "0";
+    input.value = "";
+    history.textContent = "";
 });
 
 const btn_kq = document.querySelector('.btn-kq');
@@ -42,9 +55,12 @@ btn_kq.addEventListener('click',function(){
     string_number += input.value;
     history.textContent = string_number;
     clear_number();
-    string_number ="" + eval(string_number);
-    console.log(string_number);
-    input.value += string_number;
+    try {
+        string_number ="" + eval(string_number);   
+        input.value += string_number; 
+    } catch (error) {
+        input.value = "Lỗi Phép Tính";
+    }
     string_number ="";
 });
 
@@ -54,3 +70,16 @@ function check() {
     }
     return 0;
 }
+
+function remove_last(params) {
+    let term = "";
+    for (let index = 0; index < params.length-1; index++) {
+        term += params[index];
+    }
+    return term;
+}
+
+const btn_float = document.querySelector(".btn-float");
+btn_float.addEventListener('click', function(element){
+    input.value += ".";
+});
